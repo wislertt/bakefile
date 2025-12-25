0e6ebda293905a319340c67e88cdf69d200e6cee
Wisaroot Lertthaweedech <l.wisaroot@gmail.com>
2025-12-25 15:39:35 +0700

    feat: dev

--- tests/cli/bake/test_main.py
+++ tests/cli/bake/test_main.py
@@ -20,3 +20,17 @@ class TestMain:
         assert "--chdir" in captured.out and "-C" in captured.out
         assert "--file-name" in captured.out and "-f" in captured.out
         assert "--book-name" in captured.out and "-b" in captured.out
+
+    def test_main_help2(
+        self,
+        examples_no_bakebook_dir: Path,
+        monkeypatch: pytest.MonkeyPatch,
+        capsys: pytest.CaptureFixture[str],
+    ) -> None:
+        monkeypatch.setattr(sys, "argv", ["bake", "--help", "-C", str(examples_no_bakebook_dir)])
+        with pytest.raises(SystemExit):
+            main()
+        captured = capsys.readouterr()
+        assert "--chdir" in captured.out and "-C" in captured.out
+        assert "--file-name" in captured.out and "-f" in captured.out
+        assert "--book-name" in captured.out and "-b" in captured.out
--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,15 @@ import pytest
 
 
 @pytest.fixture(scope="session")
+def examples_no_bakebook_dir() -> Path:
+    return Path(__file__).parent.parent / "examples" / "no_bakebook"
+
+
+@pytest.fixture(scope="session")
+def examples_no_bakefile_dir() -> Path:
+    return Path(__file__).parent.parent / "examples" / "no_bakefile"
+
+
+@pytest.fixture(scope="session")
 def examples_simple_dir() -> Path:
     return Path(__file__).parent.parent / "examples" / "simple"
