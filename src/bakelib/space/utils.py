1914394f93ce5ab0b7b8edfa9be75dbec120a75f
Wisaroot Lertthaweedech <l.wisaroot@gmail.com>
2026-01-11 08:36:48 +0700

    feat: improve remove_git_clean_candidates

--- src/bakelib/space/utils.py
+++ src/bakelib/space/utils.py
@@ -7,6 +7,27 @@ from pathspec.patterns.gitignore.basic import GitIgnoreBasicPattern
 from bake.ui import console
 
 
+def _skip_msg(path: Path, suffix: str, dry_run: bool) -> None:
+    verb = "Would skip" if dry_run else "Skipping"
+    console.echo(f"[yellow]~[/yellow] {verb} {suffix}{path}")
+
+
+def _remove_msg(path: Path, dry_run: bool) -> None:
+    verb = "Would remove" if dry_run else "Removing"
+    console.echo(f"[red]-[/red] [dim]{verb}[/dim] {path}")
+
+
+def _should_remove_path(path: Path, dry_run: bool) -> None:
+    _remove_msg(path, dry_run)
+    if dry_run:
+        return
+
+    if path.is_dir():
+        shutil.rmtree(path)
+    else:
+        path.unlink(missing_ok=True)
+
+
 def remove_git_clean_candidates(
     git_clean_dry_run_output: str, exclude_patterns: set[str], dry_run: bool
 ) -> None:
@@ -24,22 +45,11 @@ def remove_git_clean_candidates(
         path = Path(rel_path)
 
         if spec.match_file(rel_path):
-            skip_msg = "Would skip" if dry_run else "Skipping"
-            console.echo(f"[yellow]~[/yellow] {skip_msg} {path}")
+            _skip_msg(path, "", dry_run)
             continue
 
         if path.is_dir() and (path / ".git").exists():
-            skip_msg = "Would skip" if dry_run else "Skipping"
-            console.echo(f"[yellow]~[/yellow] {skip_msg} git repository {path}")
+            _skip_msg(path, "git repository ", dry_run)
             continue
 
-        remove_msg = "Would remove" if dry_run else "Removing"
-        console.echo(f"[red]-[/red] [dim]{remove_msg}[/dim] {path}")
-
-        if dry_run is True:
-            continue
-
-        if path.is_dir():
-            shutil.rmtree(path)
-        else:
-            path.unlink(missing_ok=True)
+        _should_remove_path(path, dry_run)
